6daa23b190a7bc6b156ed637dd4a6db730565856
root <root@t2vg-a100-G4-1.qi53rbp3hbaehnq5qaz51ogkfa.jx.internal.cloudapp.net>
2024-12-20 11:22:01 +0000

    updat

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
-
-
+apt-get install libglib2.0-0
+apt-get install libglib2.0-dev
 
 pip install pytest-playwright
+playwright install-deps
 playwright install
 
-pip install dreamsim
 
+pip install dreamsim
+pip install opencv-python-headless
 pip install opencv-python==4.7.0.72   
 
 pip install argparse
